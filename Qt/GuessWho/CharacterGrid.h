9909ac171c37dd718933d366ef8d9977ad01c46c
Unknown <quesnel.charles@gmail.com>
2018-04-10 15:34:38 -0400

    Added methods to connect CharacterGrid to backend

--- Qt/GuessWho/CharacterGrid.h
+++ Qt/GuessWho/CharacterGrid.h
@@ -4,26 +4,28 @@
 #include <QLabel>
 #include <vector>
 #include "CharacterCard.h"
+#include "CharacterManager.h"
 
 class CharacterGrid : public QWidget
 {
 private:
 	QGridLayout * characterGrid;
-	std::vector<CharacterCard*> *images;
+	std::vector<CharacterCard*> *characters;
 	int gridWidth;
 	int gridHeight;
 	CharacterCard* chosenCharacter;
+	CharacterManager* characterManager;
 
 public:
-	CharacterGrid(QWidget *parent, int height, int width);
+	CharacterGrid(QWidget *parent, int height, int width, CharacterManager* characterManager = NULL);
 	~CharacterGrid();
 
+	void setupCharacters(); //TODO : use character manager to add characters
+	void setupLayouts();
 	void calculateGridDimensions(int height, int width);
 
 	int getGridWidth() { return gridWidth; }
 	int getGridHeight() { return gridHeight; }
-	int getCardHeight();
 
-	std::vector<CharacterCard*>* getCharacters() { return images; }
+	std::vector<CharacterCard*>* getCharacters() { return characters; }
 };
-
